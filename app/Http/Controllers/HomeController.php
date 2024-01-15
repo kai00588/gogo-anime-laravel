f69a99774280a2073b67393871d913d36becaf8b
kai00588 <61916927+kai00588@users.noreply.github.com>
2024-01-15 12:22:26 +0000

    v1.1.1

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,11 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
 
 class HomeController extends Controller
 {
     //
     function index () {
-        return view('user.home');
+        $response = Http::get('https://anime-api.xyz/page-1');
+        $animes = $response->json();
+        return view('user.home')->with(compact('animes'));
     }
 }
